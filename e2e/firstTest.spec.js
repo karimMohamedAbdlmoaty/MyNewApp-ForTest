0fdb5a73cd3b66c3331fe322e38aa000fa019865
Amr Mohamed <a.mohamed@shiftebusiness.com>
2019-04-22 16:57:06 +0200

    first push

---
+++ e2e/firstTest.spec.js
@@ -0,0 +1,30 @@
+describe('Example', () => {
+  beforeEach(async () => {
+    await device.reloadReactNative();
+    await device.launchApp({delete: true});
+
+  });
+
+  it('detox has worked', async () => {
+    await element(by.id("for-detox")).tap();
+    await expect(element(by.id("for-detox"))).toHaveText('ForDetoxTest 19999');
+  });
+
+
+});
+
+describe('Example', () => {
+  beforeEach(async () => {
+    await device.reloadReactNative();
+    await device.launchApp({delete: true});
+
+  });
+
+  it('detox has worked', async () => {
+    await element(by.id("for-detox")).tap();
+    await expect(element(by.id("for-detox"))).toHaveText('ForDetoxTest 19999');
+  });
+
+
+
+});
